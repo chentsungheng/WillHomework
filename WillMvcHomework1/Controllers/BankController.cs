80b2c7cf92ce237c2d51d1db6ac576568523a926
chentsungheng <chentsungheng@hotmail.com>
2015-04-24 01:06:40 +0800

    客戶銀行帳戶管理CRUD

---
+++ WillMvcHomework1/Controllers/BankController.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WillMvcHomework1.Models;
+
+namespace WillMvcHomework1.Controllers
+{
+    public class BankController : Controller
+    {
+        private 客戶資料08Entities db = new 客戶資料08Entities();
+
+        // GET: Bank
+        public ActionResult Index()
+        {
+            var data = from bank
+                       in db.客戶銀行資訊
+                       where !bank.是否刪除
+                       select bank;
+
+            return View(data.ToList());
+        }
+
+        // GET: Bank/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            客戶銀行資訊 客戶銀行資訊 = db.客戶銀行資訊.Find(id);
+            if (客戶銀行資訊 == null)
+            {
+                return HttpNotFound();
+            }
+            return View(客戶銀行資訊);
+        }
+
+        // GET: Bank/Create
+        public ActionResult Create()
+        {
+            ViewBag.客戶Id = new SelectList(db.客戶資料.Where(c => !c.是否刪除), "Id", "客戶名稱");
+            return View();
+        }
+
+        // POST: Bank/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,客戶Id,銀行名稱,銀行代碼,分行代碼,帳戶名稱,帳戶號碼,是否刪除")] 客戶銀行資訊 客戶銀行資訊)
+        {
+            if (ModelState.IsValid)
+            {
+                db.客戶銀行資訊.Add(客戶銀行資訊);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.客戶Id = new SelectList(db.客戶資料, "Id", "客戶名稱", 客戶銀行資訊.客戶Id);
+            return View(客戶銀行資訊);
+        }
+
+        // GET: Bank/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            客戶銀行資訊 客戶銀行資訊 = db.客戶銀行資訊.Find(id);
+            if (客戶銀行資訊 == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.客戶Id = new SelectList(db.客戶資料.Where(c => !c.是否刪除), "Id", "客戶名稱", 客戶銀行資訊.客戶Id);
+            return View(客戶銀行資訊);
+        }
+
+        // POST: Bank/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,客戶Id,銀行名稱,銀行代碼,分行代碼,帳戶名稱,帳戶號碼,是否刪除")] 客戶銀行資訊 客戶銀行資訊)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(客戶銀行資訊).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.客戶Id = new SelectList(db.客戶資料, "Id", "客戶名稱", 客戶銀行資訊.客戶Id);
+            return View(客戶銀行資訊);
+        }
+
+        // GET: Bank/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            客戶銀行資訊 客戶銀行資訊 = db.客戶銀行資訊.Find(id);
+
+            if (客戶銀行資訊 == null)
+            {
+                return HttpNotFound();
+            }
+
+            ViewBag.BankName = 客戶銀行資訊.銀行名稱;
+
+            return View(客戶銀行資訊);
+        }
+
+        // POST: Bank/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            客戶銀行資訊 客戶銀行資訊 = db.客戶銀行資訊.Find(id);
+
+            //db.客戶銀行資訊.Remove(客戶銀行資訊);
+            客戶銀行資訊.是否刪除 = true;
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
