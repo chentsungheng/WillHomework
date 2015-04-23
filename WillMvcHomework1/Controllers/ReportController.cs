55a6e33a9b528be9a2528d7aeeb393515e5a8a53
chentsungheng <chentsungheng@hotmail.com>
2015-04-24 01:08:32 +0800

    簡易報表及驗證Email重覆

---
+++ WillMvcHomework1/Controllers/ReportController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WillMvcHomework1.Models;
+
+namespace WillMvcHomework1.Controllers
+{
+    public class ReportController : Controller
+    {
+        private 客戶資料08Entities db = new 客戶資料08Entities();
+
+        // GET: Report
+        public ActionResult Index()
+        {
+            return View(db.客戶報表.ToList());
+        }
+    }
+}
---
+++ WillMvcHomework1/Controllers/ValidationController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI;
+using WillMvcHomework1.Models;
+
+namespace WillMvcHomework1.Controllers
+{
+    [OutputCache(Location = OutputCacheLocation.None, NoStore = true)]
+    public class ValidationController : Controller
+    {
+        private 客戶資料08Entities db = new 客戶資料08Entities();
+
+        public JsonResult IsMailAvailable(string Email, int 客戶Id, int Id)
+        {
+            var data = from contact
+                       in db.客戶聯絡人
+                       where contact.Email == Email && contact.客戶Id == 客戶Id && contact.Id != Id
+                       select contact;
+
+            return Json(data.Count() == 0, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
