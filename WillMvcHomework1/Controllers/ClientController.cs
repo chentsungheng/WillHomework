58c712a165a4de8b5ad7dcf401f835f0a903d39b
chentsungheng <chentsungheng@hotmail.com>
2015-04-24 01:03:31 +0800

    客戶資料管理CRUD

---
+++ WillMvcHomework1/Controllers/ClientController.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WillMvcHomework1;
+using WillMvcHomework1.Models;
+
+namespace WillMvcHomework1.Controllers
+{
+    public class ClientController : Controller
+    {
+        private Models.客戶資料08Entities db = new 客戶資料08Entities();
+
+        // GET: Client
+        public ActionResult Index()
+        {
+            var data = from client
+                       in db.客戶資料
+                       where !client.是否刪除
+                       select client;
+
+            return View(data.ToList());
+        }
+
+        // GET: Client/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            客戶資料 客戶資料 = db.客戶資料.Find(id);
+
+            if (客戶資料 == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(客戶資料);
+        }
+
+        // GET: Client/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Client/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,客戶名稱,統一編號,電話,傳真,地址,Email")] 客戶資料 客戶資料)
+        {
+            if (ModelState.IsValid)
+            {
+                db.客戶資料.Add(客戶資料);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(客戶資料);
+        }
+
+        // GET: Client/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            客戶資料 客戶資料 = db.客戶資料.Find(id);
+            if (客戶資料 == null)
+            {
+                return HttpNotFound();
+            }
+            return View(客戶資料);
+        }
+
+        // POST: Client/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,客戶名稱,統一編號,電話,傳真,地址,Email")] 客戶資料 客戶資料)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(客戶資料).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(客戶資料);
+        }
+
+        // GET: Client/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            客戶資料 客戶資料 = db.客戶資料.Find(id);
+
+            if (客戶資料 == null)
+            {
+                return HttpNotFound();
+            }
+
+            ViewBag.CompanyName = 客戶資料.客戶名稱;
+
+            return View(客戶資料);
+        }
+
+        // POST: Client/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            客戶資料 客戶資料 = db.客戶資料.Find(id);
+            //db.客戶資料.Remove(客戶資料);
+            客戶資料.是否刪除 = true;
+
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
