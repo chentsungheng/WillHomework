5f1662bdf014b201d655a711039b2e8ed310bf21
chentsungheng <chentsungheng@hotmail.com>
2015-04-24 01:05:26 +0800

    客戶聯絡人管理CRUD

---
+++ WillMvcHomework1/Controllers/ContactController.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WillMvcHomework1.Models;
+
+namespace WillMvcHomework1.Controllers
+{
+    public class ContactController : Controller
+    {
+        private 客戶資料08Entities db = new 客戶資料08Entities();
+
+        // GET: Contact
+        public ActionResult Index()
+        {
+            var data = from contact
+                       in db.客戶聯絡人
+                       where !contact.是否刪除
+                       select contact;
+
+            return View(data.ToList());
+        }
+
+        // GET: Contact/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            客戶聯絡人 客戶聯絡人 = db.客戶聯絡人.Find(id);
+            if (客戶聯絡人 == null)
+            {
+                return HttpNotFound();
+            }
+            return View(客戶聯絡人);
+        }
+
+        // GET: Contact/Create
+        public ActionResult Create()
+        {
+            var data = from client
+                       in db.客戶資料
+                       where !client.是否刪除
+                       select client;
+
+            ViewBag.客戶Id = new SelectList(data, "Id", "客戶名稱");
+            return View();
+        }
+
+        // POST: Contact/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,客戶Id,職稱,姓名,Email,手機,電話,是否刪除")] 客戶聯絡人 客戶聯絡人)
+        {
+            if (ModelState.IsValid)
+            {
+                db.客戶聯絡人.Add(客戶聯絡人);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.客戶Id = new SelectList(db.客戶資料, "Id", "客戶名稱", 客戶聯絡人.客戶Id);
+            return View(客戶聯絡人);
+        }
+
+        // GET: Contact/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            客戶聯絡人 客戶聯絡人 = db.客戶聯絡人.Find(id);
+
+            if (客戶聯絡人 == null)
+            {
+                return HttpNotFound();
+            }
+
+            var data = from client
+                       in db.客戶資料
+                       where !client.是否刪除
+                       select client;
+
+            ViewBag.客戶Id = new SelectList(data, "Id", "客戶名稱", 客戶聯絡人.客戶Id);
+            return View(客戶聯絡人);
+        }
+
+        // POST: Contact/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,客戶Id,職稱,姓名,Email,手機,電話,是否刪除")] 客戶聯絡人 客戶聯絡人)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(客戶聯絡人).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.客戶Id = new SelectList(db.客戶資料, "Id", "客戶名稱", 客戶聯絡人.客戶Id);
+            return View(客戶聯絡人);
+        }
+
+        // GET: Contact/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            客戶聯絡人 客戶聯絡人 = db.客戶聯絡人.Find(id);
+
+            if (客戶聯絡人 == null)
+            {
+                return HttpNotFound();
+            }
+
+            ViewBag.Name = 客戶聯絡人.姓名;
+
+            return View(客戶聯絡人);
+        }
+
+        // POST: Contact/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            客戶聯絡人 客戶聯絡人 = db.客戶聯絡人.Find(id);
+
+            //db.客戶聯絡人.Remove(客戶聯絡人);
+            客戶聯絡人.是否刪除 = true;
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
